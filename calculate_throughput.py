ed94556fe162854cb006bd5e1afb064582866884
Rebecca Wang <rwang7@stanford.edu>
2017-05-28 22:48:11 -0700

    add consistent error label to calculate_throughput

--- calculate_throughput.py
+++ calculate_throughput.py
@@ -11,17 +11,17 @@ args = parser.parse_args()
 def calculate_throughput(fname):
 	lines = open(fname).readlines()
 	if len(lines) == 0:
-		print "Empty file, check what's going on"
+		print "error: Empty file, check what's going on"
 		return
 	last_line = lines[len(lines) - 1]
 	if ' 0.0' not in last_line:
-		print "Could not find line in throughput file with aggregate throughput"
+		print "error: Could not find line in throughput file with aggregate throughput"
 		return
 	
 	m = re.search('(?<=Bytes)[0-9\.\ ]+', last_line)
 
 	if m is None:
-		print 'Last line does not match the regex'
+		print 'error: Last line does not match the regex'
 		return
 	else:
 		if 'Mbits' in last_line:
