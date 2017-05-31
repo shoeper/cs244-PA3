09bc01d33e1b2d6db80f60ee43e88f174351813b
Hope <hcaseyal@stanford.edu>
2017-05-30 20:03:10 -0700

    update

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 time=60
 iperf_port=5001
 
-for interburst_period in 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.5 3.0 3.5 4.0 4.5 5.0; do
-	for qsize in 3 4 5 6 7 8 9 10 12 14 16 18 20; do
+for interburst_period in 0.2 0.25 0.3 0.35 0.4 0.45 0.50 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.2; do
+	for qsize in 4; do
 		for i in 1 2 3; do
 		    echo "Running cs244 PA3 for queue size $qsize :)"
 		    sudo mn --clean
